87ca0c1a8feaf67b980c5f20193ebed1deed741e
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2025-09-26 23:32:30 -0400

    Added pkg dependencies to the .xml file

--- diffrobot_pkg/setup.py
+++ diffrobot_pkg/setup.py
@@ -19,7 +19,7 @@ setup(
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+        'console_scripts': [ #TODO - ADD ENTRY POINTS FOR EXECUTABLES
         ],
     },
 )
