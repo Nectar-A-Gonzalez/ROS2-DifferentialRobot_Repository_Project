0d13895280e121c2f8880ae5b882ffb9a9224557
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2025-10-02 15:45:00 -0400

    Set the robot test case parameters

--- diffrobot_pkg/diffrobot_pkg/encoder_node.py
+++ diffrobot_pkg/diffrobot_pkg/encoder_node.py
@@ -30,11 +30,11 @@ from diffrobot_interfaces.msg import WheelTicks #custom pkg
 # int32 left_ticks
 # int32 right_ticks
 
-# Variables for configuration (CHANGEABLE):
+# Variables for configuration (CHANGEABLE): #TODO-MENTION THIS IN THE README and give the test values
 # These do not varie from velocity command to velocity command
-wheel_radius = DFDF #(meters)
-wheel_axel_width = DFDF #(meters)
-encoder_resolution = DFDF #Pulses per Rotation PPR
+wheel_radius = 0.0889 #(meters) 
+wheel_axel_width = 0.2032 #(meters)
+encoder_resolution = 500 #Pulses per Rotation PPR
 t = 1 #(seconds) #time the velocity is applied for
 
 class EncoderNode(Node): 
