3037c47c5db8b67e4b78dae5b42b08e4cb95872d
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2025-10-03 15:45:19 -0400

    Future improvements notes - for robot parameter implementation

--- diffrobot_pkg/diffrobot_pkg/robot_parameters.py
+++ diffrobot_pkg/diffrobot_pkg/robot_parameters.py
@@ -5,3 +5,9 @@ wheel_radius = 0.0889 #(meters)
 wheel_axel_width = 0.2032 #(meters)
 encoder_resolution = 500 #Pulses per Rotation PPR
 t = 1 #(seconds) #time the velocity is applied for
+
+#Future improvements - these as parameters for
+#the launch file might be recommended. So that they
+#be changed directly from the command line and all nodes
+#still use the same values. 
+#https://docs.ros.org/en/jazzy/Tutorials/Intermediate/Launch/Using-ROS2-Launch-For-Large-Projects.html
