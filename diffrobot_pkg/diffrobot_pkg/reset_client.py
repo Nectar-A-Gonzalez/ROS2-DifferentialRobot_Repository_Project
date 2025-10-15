01aa1a62c966cc0a2ba3b364441d333d5d040d21
Nectar-A-Gonzalez <nectargonzalez@outlook.com>
2025-10-15 15:11:20 -0400

    Reset Client logger info per case

--- diffrobot_pkg/diffrobot_pkg/reset_client.py
+++ diffrobot_pkg/diffrobot_pkg/reset_client.py
@@ -2,8 +2,6 @@
 # Tasks:
 # - Calls /reset_pose with values from CLI args and prints the result. 
 
-# srv message example:
-
 import sys #To use command line values
 import rclpy
 from rclpy.node import Node
@@ -15,8 +13,7 @@ class ResetClient(Node):
         super.__init__("reset_client")
         self.cli = self.create_client(SetPose, 'reset_pose') # srv type, srv comm channel name, have to match
 
-        #~?? Time it will wait for, and if this function doesnt come back true aka the service is not active/available, it will keep in this loop.. waiting 
-        # It is a boolean #TODO Verify***
+        #~?? Time it will wait for, and if this function doesnt come back true aka the service is not active/available, it will keep in this loop.. waiting
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...') #Stores in logger, not print TODO VERIFY
         
@@ -35,13 +32,29 @@ class ResetClient(Node):
 
 def main(args=None):
     rclpy.init(args=args) #Allows that ROS2 commands given at command line pass to ROS itself
-    rclpy.spin.once(reset_client) #Use established name attribute
-    if reset_client.futute.done(): #Accesing the attributes with self aka the name
-        try:
-            response = reset_client.future.result() #future stores result after request to server
-        except Exception as e:
-            reset_client.get_logger().info(
-                'Service call failed %r' %
-            )            
-# TODO - Check IPad for best of the two client.py formats
-# and use the recommended one - the one from custom interfaces is better
+    reset_client = ResetClient()
+    reset_client.send_request() 
+    while rclpy.ok():
+        rclpy.spin.once(reset_client) #Use established name attribute
+        if reset_client.futute.done(): #Accesing the attributes with self aka the name
+            try:
+                response = reset_client.future.result() #future stores result after request to server
+            except Exception as e:
+                reset_client.get_logger().info(
+                    'Service call failed %r' %(e,))
+            else:
+                reset_client.get_logger().info(
+                    'Result of result client: Requested position [x:%d, y:%d, theta:%d]; Request has been accepted: %d; Status of the Robot: %d' %
+                    (reset_client.request.x, reset_client.request.y, reset_client.request.theta, response.accepted, response.status)
+                    
+                    #Use values of srv not actual for x,y,theta for node
+                    # Need to give value to status in the server and the accepted also, 
+                    # Just put the pure response variables as info for the logger
+                )
+            break
+
+    reset_client.destroy_node()
+    rclpy.shutdown()
+
+    if __name__ == '__main__':
+        main()
